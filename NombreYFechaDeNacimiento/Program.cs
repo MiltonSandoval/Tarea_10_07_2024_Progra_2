d368959f317e2cc00e2547611e730b18c3579238
Milton <sandovalmonasteriom@gmail.com>
2024-07-10 14:43:07 -0400

    Se creo  y se probo el programa:  "NombreYFechaDeNacimiento"

---
+++ NombreYFechaDeNacimiento/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Tarea1
+{
+    class Programa
+    {
+        static void Main(string[] args)
+        {
+            string Nombre, Dia, Mes, Año;
+            Console.WriteLine("BIENVENIDO");
+            Console.Write("Su nombre:");
+            Nombre = Console.ReadLine();
+            Console.WriteLine("Fecha de nacimiento:");
+            Console.Write("Dia:");
+            Dia = Console.ReadLine();
+            
+            Console.Write("Mes:");
+            Mes = Console.ReadLine();
+
+            Console.Write("Año:");
+            Año = Console.ReadLine();
+
+            Console.WriteLine($"{Nombre}, {Dia}/{Mes}/{Año} \n");
+        }
+    }
+}
